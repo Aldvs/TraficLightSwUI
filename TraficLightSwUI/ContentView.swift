3457bd1287db5fcb36b6dfd6bfb40d35072252fb
aldvs <andrewgogol777@gmail.com>
2022-02-16 22:19:39 +0300

    Setup logic of TrafficLight in ContentView

--- TraficLightSwUI/ContentView.swift
+++ TraficLightSwUI/ContentView.swift
@@ -13,14 +13,14 @@ enum CurrentLight {
 
 struct ContentView: View {
     
-    let attributedString = try! AttributedString(
+    @State private var attributedString = try! AttributedString(
         markdown: "START")
     
-    @State var redOpacity: Double = 0.5
-    @State var yellowOpacity: Double = 0.5
-    @State var greenOpacity: Double = 0.5
+    @State private var redOpacity: Double = 0.5
+    @State private var yellowOpacity: Double = 0.5
+    @State private var greenOpacity: Double = 0.5
     
-    @State var currentLight = CurrentLight.red
+    @State private var currentLight = CurrentLight.red
     private let lightIsOn: Double = 1
     private let lightIsOff: Double = 0.5
     
@@ -28,18 +28,13 @@ struct ContentView: View {
         VStack{
             ColorCircle(color: .red.opacity(redOpacity))
                 .padding()
-                .id("red")
             ColorCircle(color: .yellow.opacity(yellowOpacity))
                 .padding()
-                .id("yellow")
             ColorCircle(color: .green.opacity(greenOpacity))
                 .padding()
-                .id("green")
             Spacer()
             
-            Button(action: {
-                
-            }) {
+            Button(action: { getChangedLight() }) {
                 textSettings
             }
             .shadow(radius: 30)
@@ -60,6 +55,25 @@ struct ContentView: View {
                 RoundedRectangle(cornerRadius: 20)
                     .stroke(Color.white, lineWidth: 5)
             )
+    }
+    
+    private func getChangedLight() {
+        attributedString = "NEXT"
+        
+        switch currentLight {
+        case .red:
+            greenOpacity = lightIsOff
+            redOpacity = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redOpacity = lightIsOff
+            yellowOpacity = lightIsOn
+            currentLight = .blue
+        case .blue:
+            yellowOpacity = lightIsOff
+            greenOpacity = lightIsOn
+            currentLight = .red
+        }
     }
 }
 
