57521e0825f1d92737fce4c814b34d38f4fbb695
aldvs <andrewgogol777@gmail.com>
2022-02-16 15:45:50 +0300

    Initial Commit

---
+++ TraficLightSwUI/TraficLightSwUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightSwUIApp.swift
+//  TraficLightSwUI
+//
+//  Created by admin on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightSwUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
