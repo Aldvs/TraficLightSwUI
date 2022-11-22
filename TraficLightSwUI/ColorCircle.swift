f9403b611b5f6ec94aeab6ba540366e918c31a97
aldvs <andrewgogol777@gmail.com>
2022-11-22 20:51:47 +0300

    1. Update ColorCircle struct

--- TraficLightSwUI/ColorCircle.swift
+++ TraficLightSwUI/ColorCircle.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ColorCircle: View {
+    
     let color: Color
+    let opacity: Double
     
     var body: some View {
         Circle()
             .frame(width: 125, height: 125)
             .foregroundColor(color)
+            .opacity(opacity)
             .overlay(Circle().stroke(Color.white, lineWidth: 7))
             .shadow(radius: 14)
     }
@@ -21,6 +24,6 @@ struct ColorCircle: View {
 
 struct ColorCircle_Previews: PreviewProvider {
     static var previews: some View {
-        ColorCircle(color: .indigo)
+        ColorCircle(color: .indigo, opacity: 1)
     }
 }
